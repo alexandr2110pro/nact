8a0e27e1e26e778472aa2ee420ed07e870530db7
Nick Cuthbert <nick@cuthbert.co.za>
2018-01-07 23:26:00 +0200

    Simplified code

--- test/freeze.js
+++ test/freeze.js
@@ -3,69 +3,67 @@
 var freeze = require('../lib/freeze');
 require('chai').should();
 
-describe('deep-freeze', function () {
-  describe('init', function () {
-    let person = {};
-    let expectedPerson = {};
+describe('freeze', function () {
+  let person = {};
+  let expectedPerson = {};
 
-    beforeEach(function () {
-      person = {
-        name: 'John',
-        surname: 'Johnson',
-        age: 26,
-        address: {
-          street: '1st Street',
-          city: 'Los Angeles',
-          country: 'USA'
-        },
-        vehicles: ['BMW', 'Ferrari', 'Lamborghini']
-      };
-
-      expectedPerson = {
-        name: 'John',
-        surname: 'Johnson',
-        age: 26,
-        address: {
-          street: '1st Street',
-          city: 'Los Angeles',
-          country: 'USA'
-        },
-        vehicles: ['BMW', 'Ferrari', 'Lamborghini']
-      };
-    });
-
-    it('should not override any object properties', function () {
-      person = freeze(person);
-      person.name = 'Jack';
-      person.surname = 'Jackson';
-      person.age = 18;
-      person.address = {
-        street: 'Third Street',
-        city: 'San Francisco',
+  beforeEach(function () {
+    person = {
+      name: 'John',
+      surname: 'Johnson',
+      age: 26,
+      address: {
+        street: '1st Street',
+        city: 'Los Angeles',
         country: 'USA'
-      };
-      person.vehicles = ['Toyota'];
-      person.name = 'Jack';
-      person.surname = 'Jackson';
-      person.age = 18;
-      person.address.street = 'Third Street';
-      person.address.city = 'San Francisco';
-      person.address.country = 'USA';
-      person.vehicles[0] = 'Toyota';
+      },
+      vehicles: ['BMW', 'Ferrari', 'Lamborghini']
+    };
 
-      delete person.vehicles[1];
-      delete person.vehicles[2];
+    expectedPerson = {
+      name: 'John',
+      surname: 'Johnson',
+      age: 26,
+      address: {
+        street: '1st Street',
+        city: 'Los Angeles',
+        country: 'USA'
+      },
+      vehicles: ['BMW', 'Ferrari', 'Lamborghini']
+    };
+  });
 
-      person.should.deep.equal(expectedPerson, 'any object properties should not be overridden');
-    });
+  it('should not override any object properties', function () {
+    person = freeze(person);
+    person.name = 'Jack';
+    person.surname = 'Jackson';
+    person.age = 18;
+    person.address = {
+      street: 'Third Street',
+      city: 'San Francisco',
+      country: 'USA'
+    };
+    person.vehicles = ['Toyota'];
+    person.name = 'Jack';
+    person.surname = 'Jackson';
+    person.age = 18;
+    person.address.street = 'Third Street';
+    person.address.city = 'San Francisco';
+    person.address.country = 'USA';
+    person.vehicles[0] = 'Toyota';
 
-    it('should not add new properties to an object', function () {
-      person = freeze(person);
+    delete person.vehicles[1];
+    delete person.vehicles[2];
 
-      person.occupation = 'Lawyer';
-      person.nickname = 'JJ';
+    person.should.deep.equal(expectedPerson, 'any object properties should not be overridden');
+  });
 
-      person.should.deep.equal(expectedPerson);
-    });
+  it('should not add new properties to an object', function () {
+    person = freeze(person);
+
+    person.occupation = 'Lawyer';
+    person.nickname = 'JJ';
+
+    person.should.deep.equal(expectedPerson);
   });
 });
